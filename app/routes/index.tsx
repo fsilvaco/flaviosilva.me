87974ae25b7f0580b4c2bd80581502bd3bc26078
Flavio Silva <flavio.silva@g.globo>
2022-04-29 11:41:35 -0300

    Add initial Remix App Framework

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,3 +3,7 @@ import type { LoaderFunction } from "@remix-run/node";
 export const loader: LoaderFunction = async ({ params }) => {
   return {};
 };
+
+export default function () {
+  return <h1>hello</h1>;
+}
