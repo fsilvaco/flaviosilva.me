67e54f1b522bb381b6621e68188833080490706d
Flavio Silva <flavio.silva@g.globo>
2022-06-22 20:21:12 -0300

    Add Tailwindcss configuration

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,17 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
+  Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import styles from "./tailwind.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: styles }];
+};
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -22,6 +28,7 @@ export default function App() {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌿</text></svg>"
         />
+        <Links />
       </head>
       <body>
         <Outlet />
